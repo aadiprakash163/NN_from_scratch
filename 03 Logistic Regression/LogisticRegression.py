aeef11fe1e5add2d9b30a1f1c6ac15623d54656f
Aaditya Prakash Chouhan <aadiprakash163@gmail.com>
2025-02-07 15:35:55 +0530

    First commit
    	1. Some untracked files were there
    	2. Some modified files were there
    	3. Some Delete files were there

--- 03 Logistic Regression/LogisticRegression.py
+++ 03 Logistic Regression/LogisticRegression.py
@@ -3,11 +3,11 @@ import numpy as np
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
+class LogisticRegression():
 
-class LogisticRegression:
-    def __init__(self, lr = 0.001, n_iter = 1000):
+    def __init__(self, lr=0.001, n_iters=1000):
         self.lr = lr
-        self.n_iter = n_iter
+        self.n_iters = n_iters
         self.weights = None
         self.bias = None
 
@@ -16,20 +16,19 @@ class LogisticRegression:
         self.weights = np.zeros(n_features)
         self.bias = 0
 
-        for _ in range(self.n_iter):
+        for _ in range(self.n_iters):
             linear_pred = np.dot(X, self.weights) + self.bias
-            y_pred = sigmoid(linear_pred)
+            predictions = sigmoid(linear_pred)
 
-            dw = (1/n_samples) * np.dot(X.T, (y_pred - y))
-            db = (1/n_samples) * np.sum(y_pred - y)
+            dw = (1/n_samples) * np.dot(X.T, (predictions - y))
+            db = (1/n_samples) * np.sum(predictions-y)
 
-            self.weights = self.weights -self.lr * dw
-            self.bias = self.bias - self.lr * db
+            self.weights = self.weights - self.lr*dw
+            self.bias = self.bias - self.lr*db
+
 
     def predict(self, X):
         linear_pred = np.dot(X, self.weights) + self.bias
         y_pred = sigmoid(linear_pred)
-
         class_pred = [0 if y<=0.5 else 1 for y in y_pred]
-
         return class_pred
--- 04 Decision Trees/DecisionTree.py
+++ 04 Decision Trees/DecisionTree.py
@@ -111,3 +111,6 @@ class DecisionTree:
         if x[node.feature] <= node.threshold:
             return self._traverse_tree(x, node.left)
         return self._traverse_tree(x, node.right)
+        
+
+
